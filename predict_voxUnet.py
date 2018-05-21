c95116972f3ea926daf4218f28056e61205f56fb
Limeng <pulimeng0722@gmail.com>
2018-05-21 17:26:02 -0500

    Add files via upload

---
+++ predict_voxUnet.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 15 16:48:58 2018
+
+@author: limeng
+"""
+
+import numpy as np
+
+from keras.models import load_model
+from keras.utils import plot_model
+import keras.backend as K
+
+def tversky_loss(y_true, y_pred):
+    alpha = 0.9
+    beta  = 0.1
+    
+    ones = K.ones(K.shape(y_true))
+    p0 = y_pred      # proba that voxels are class i
+    p1 = ones-y_pred # proba that voxels are not class i
+    g0 = y_true
+    g1 = ones-y_true
+    
+    num = K.sum(p0*g0, (0,1,2,3))
+    den = num + alpha*K.sum(p0*g1,(0,1,2,3)) + beta*K.sum(p1*g0,(0,1,2,3))
+    
+    T = K.sum(num/den) # when summing over classes, T has dynamic range [0 Ncl]
+    
+    Ncl = K.cast(K.shape(y_true)[-1], 'float32')
+    return Ncl-T
+
+def weighted_dice_coef(y_true, y_pred, smooth=1e-7):
+    y_true_0 = K.flatten(K.one_hot(K.cast(y_true, 'int64'), num_classes=15)[...,0])
+    y_pred_0 = K.flatten(y_pred[...,0])
+    intersect_0 = K.sum(y_true_0 * y_pred_0, axis=-1)
+    denom_0 = K.sum(y_true_0 + y_pred_0, axis=-1)
+    y_true_1 = K.flatten(K.one_hot(K.cast(y_true, 'int64'), num_classes=15)[...,1:])
+    y_pred_1 = K.flatten(y_pred[...,1:])
+    intersect_1 = K.sum(y_true_1 * y_pred_1, axis=-1)*10000
+    denom_1 = K.sum(y_true_1 + y_pred_1, axis=-1)*10000
+    intersect = intersect_0 + intersect_1
+    denom = denom_0 + denom_1
+    return K.mean((2. * intersect / (denom + smooth)))
+
+def weighted_dice_coef_loss(y_true, y_pred):
+    '''
+    Dice loss to minimize. Pass to model as loss during compile statement
+    '''
+    return 1 - weighted_dice_coef(y_true, y_pred)
+
+def dice_coef(y_true, y_pred, smooth=1e-7):
+    y_true_f = K.flatten(K.one_hot(K.cast(y_true, 'int64'), num_classes=15))
+    y_pred_f = K.flatten(y_pred)
+    intersect = K.sum(y_true_f * y_pred_f, axis=-1)
+    denom = K.sum(y_true_f + y_pred_f, axis=-1)
+    return K.mean((2. * intersect / (denom + smooth)))
+
+def dice_coef_loss(y_true, y_pred):
+    '''
+    Dice loss to minimize. Pass to model as loss during compile statement
+    '''
+    return 1 - dice_coef(y_true, y_pred)
+
+# make a line for the .pdb file
+def makeLine(coord, k):
+    atom = 'ATOM'
+    atom_sn = str(k+1)
+    atom_name = 'D1'
+    res_name = 'LIG'
+    E = 0.1
+    x = '{:.3f}'.format(round(coord[0], 3))
+    y = '{:.3f}'.format(round(coord[1], 3))
+    z = '{:.3f}'.format(round(coord[2], 3))
+    EE = '{:.2f}'.format(round(E,2))
+    string = atom + ' '*2 + '{:>5}'.format(atom_sn) + ' ' + '{:4}'.format(atom_name) + ' ' \
+            + '{:>3}'.format(res_name) + ' '*2 + '   1' + ' '*4 + '{:>8}'.format(x) + '{:>8}'.format(y) + '{:>8}'.format(z) \
+            + '{:>6}'.format('1.00') + '{:>6}'.format(EE) + ' '*8 + '\n'
+    return string
+
+protein_name = '13pkA'
+data_folder = '/home/limeng/Desktop/pocket_similarity/model/voxel/voxelization/voxel_data/'
+data_path = data_folder + protein_name + '.npy'
+
+pocket_voxel = np.load(data_path)
+X = np.transpose(pocket_voxel, (1,2,3,0))
+X = np.expand_dims(X, axis = 0)
+u_mdl = load_model('./voxUnet_models/DeepDrug_Unet_ver0.h5', custom_objects={'tversky_loss': tversky_loss,
+                                                                           'weighted_dice_coef':weighted_dice_coef,
+                                                                           'dice_coef_loss':dice_coef_loss})
+plot_model(u_mdl, show_shapes=True, to_file='VoxUnet.png')
+ligand_voxel = u_mdl.predict(X)
+y = np.squeeze(ligand_voxel)
+
+voxel_start = -15
+voxel_end = 16
+l = []
+for a in xrange(32):
+    for b in xrange(32):
+        for c in xrange(32):
+            tempy = y[a,b,c,:]
+            at = np.argmax(tempy)
+            temp = [a + voxel_start,b + voxel_start,c + voxel_start, at]
+            l.append(temp)
+            
+atom_type_list = ['C.2','C.3','C.ar','F','N.am','N.2','O.co2','N.ar','S.3','O.2','O.3','N.4','P.3','N.pl3']
+with open('./13pkA_UNet.pdb','w') as in_strm:
+    for k in xrange(len(l)):
+        type_code = l[k][3]
+#        if type_code != 0 and type_code != 12 and type_code != 13:
+        if type_code != 0:
+            print(atom_type_list[type_code-1])
+            temp_c = l[k][0:3]
+            temp_string = makeLine(temp_c, k)
+            in_strm.write(temp_string)
+        else:
+            continue
+in_strm.close()
+del u_mdl
+K.clear_session()
---
+++ train_voxUnet.py
@@ -0,0 +1,140 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Train the VoxResNet
+
+@author: limeng
+"""
+
+import os
+os.environ['KERAS_BACKEND']='tensorflow'
+
+import itertools
+import numpy as np
+from scipy import interp
+
+#import h5py
+import time
+
+import keras.backend as K
+from keras.utils import np_utils
+from keras import optimizers
+from keras import callbacks
+from keras.utils import print_summary
+from voxUnet import voxUnet
+
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_curve, auc
+
+seed = 12306
+np.random.seed(seed)
+
+def generate_batch_data_random(x, y, batch_size):
+    ylen = len(y)
+    loopcount = ylen // batch_size
+    while (True):
+        i = np.random.randint(0,loopcount)
+        yield x[i * batch_size:(i + 1) * batch_size], y[i * batch_size:(i + 1) * batch_size]
+        
+def tversky_loss(y_true, y_pred):
+    alpha = 0.9
+    beta  = 0.1
+    
+    ones = K.ones(K.shape(y_true))
+    p0 = y_pred      # proba that voxels are class i
+    p1 = ones-y_pred # proba that voxels are not class i
+    g0 = y_true
+    g1 = ones-y_true
+    
+    num = K.sum(p0*g0, (0,1,2,3))
+    den = num + alpha*K.sum(p0*g1,(0,1,2,3)) + beta*K.sum(p1*g0,(0,1,2,3))
+    
+    T = K.sum(num/den) # when summing over classes, T has dynamic range [0 Ncl]
+    
+    Ncl = K.cast(K.shape(y_true)[-1], 'float32')
+    return Ncl-T
+
+def weighted_dice_coef(y_true, y_pred, smooth=1e-7):
+    y_true_0 = K.flatten(K.one_hot(K.cast(y_true, 'int64'), num_classes=15)[...,0])
+    y_pred_0 = K.flatten(y_pred[...,0])
+    intersect_0 = K.sum(y_true_0 * y_pred_0, axis=-1)
+    denom_0 = K.sum(y_true_0 + y_pred_0, axis=-1)
+    y_true_1 = K.flatten(K.one_hot(K.cast(y_true, 'int64'), num_classes=15)[...,1:])
+    y_pred_1 = K.flatten(y_pred[...,1:])
+    intersect_1 = K.sum(y_true_1 * y_pred_1, axis=-1)*10000
+    denom_1 = K.sum(y_true_1 + y_pred_1, axis=-1)*10000
+    intersect = intersect_0 + intersect_1
+    denom = denom_0 + denom_1
+    return K.mean((2. * intersect / (denom + smooth)))
+
+def weighted_dice_coef_loss(y_true, y_pred):
+    '''
+    Dice loss to minimize. Pass to model as loss during compile statement
+    '''
+    return 1 - weighted_dice_coef(y_true, y_pred)
+
+def dice_coef(y_true, y_pred, smooth=1e-7):
+    y_true_f = K.flatten(K.one_hot(K.cast(y_true, 'int64'), num_classes=15)[...,1:])
+    y_pred_f = K.flatten(y_pred[...,1:])
+    intersect = K.sum(y_true_f * y_pred_f, axis=-1)
+    denom = K.sum(y_true_f + y_pred_f, axis=-1)
+    return K.mean((2. * intersect / (denom + smooth)))
+
+def dice_coef_loss(y_true, y_pred):
+    '''
+    Dice loss to minimize. Pass to model as loss during compile statement
+    '''
+    return 1 - dice_coef(y_true, y_pred)
+
+
+# load the ATP data
+atps = []
+with open('./atp_names.lst') as atp_in:
+    for line in atp_in.readlines():
+        temp = line.replace(' ','').replace('\n','')
+        atps.append(temp)
+# conver data into a single matrix
+voxel_folder = './voxel_data/'
+voxel = np.zeros(shape = (1553, 32, 32, 32, 14),
+    dtype = np.float64)
+label_folder = './ligand-voxel/'
+label = np.zeros(shape = (1553, 32, 32, 32, 1),
+    dtype = np.int64)
+cnt = 0
+ss = time.time()
+print '...Loading the data'
+for atp in atps:
+    v_path = voxel_folder + atp + '.npy'
+    l_path = label_folder + atp + '.npy'
+    v = np.load(v_path)
+    l = np.load(l_path)
+    v = np.transpose(v, (1,2,3,0))
+    l = np.transpose(l, (1,2,3,0))
+    voxel[cnt,:] = v
+    label[cnt,:] = l
+    cnt += 1
+print 'compuation time for data conversion is ' + str(time.time()-ss)
+
+batch_size = 32
+epoch = 30
+X_train, X_test, y_train, y_test = train_test_split(voxel, label)
+
+model = voxUnet.build((32, 32, 32, 14), 15, 1e-4)
+print_summary(model, line_length=120, positions=None, print_fn=None)
+
+opt = optimizers.Adam(lr=0.001)
+model.compile(loss = dice_coef_loss, optimizer = opt, metrics = ['sparse_categorical_accuracy', tversky_loss])
+tfCallBack = callbacks.TensorBoard(log_dir='./graph', histogram_freq = 0, batch_size=batch_size, write_graph=True, 
+					write_grads=False, write_images=True, embeddings_freq=0, embeddings_layer_names=None, embeddings_metadata=None)
+model.fit_generator(generate_batch_data_random(X_train, y_train, batch_size),                                                      
+    steps_per_epoch=len(y_train)//batch_size,
+    epochs=epoch,
+    verbose=2,
+    callbacks=[tfCallBack])
+#model.fit(X_train, y_train, batch_size=10, epochs = 1, shuffle = True, 
+#          callbacks = [tfCallBack], verbose = 2)
+scores = model.evaluate(X_test, y_test,verbose = 1)
+print(scores)
+model.save('saved_models/DeepDrug_Unet_ver0.h5')
